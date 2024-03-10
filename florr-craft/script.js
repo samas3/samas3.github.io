8f59484514cd9039798a4fee200b2c229a37306a
samas3 <samas3@163.com>
2024-03-10 11:30:42 +0800

    update algo

--- florr-craft/script.js
+++ florr-craft/script.js
@@ -4,6 +4,14 @@ var boost = [1, 1.1, 1.2];
 $('calc').onclick = () => {
   var type = $('type').value, eboost = $('boost').value, k = $('cnt').value;
   var p = rate[type] * boost[eboost] / 100;
-  var outNum = 2 * p * k / (5 * (p + 1)), out1 = 1 - (1 - p) ** ((k - (1 - p) * k / 2) / 5);
-  $('result').innerHTML = `平均消耗 ${Math.floor((2.5 + 2.5 / p) * 100) / 100} 个花瓣合成一个，期望得到 ${Math.floor(outNum * 100) / 100} 个花瓣`;
+  var outNum = 2 * p * k / (5 * (p + 1)), out1 = calc(k, p);
+  $('result').innerHTML = `平均消耗 ${Math.floor((2.5 + 2.5 / p) * 100) / 100} 个花瓣合成一个，期望得到 ${Math.floor(outNum * 100) / 100} 个花瓣，成功率 ${Math.floor(out1 * 10000) / 100}%`;
 };
+function count(k){
+  var i = Math.floor(k / 5), j = k % 5;
+  if(k < 5) return 0;
+  return i + count(5 * i / 2 + j)
+}
+function calc(k, p){
+  return 1 - (1 - p) ** count(k);
+}
