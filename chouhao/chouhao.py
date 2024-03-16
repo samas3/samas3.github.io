bf50aa584d54543e65864ad1b67246a7944485fa
samas3 <samas3@163.com>
2024-03-16 19:05:23 +0800

    add chouhao

---
+++ chouhao/chouhao.py
@@ -0,0 +1,183 @@
+from tkinter import *
+from tkinter import filedialog
+from tkinter import messagebox
+from tkinter.font import Font
+import csv
+import random
+import test
+import io
+import pyttsx3
+import threading
+import requests
+import os
+t = Tk()
+t.resizable(0, 0)
+t.wm_attributes('-topmost', 1)
+r = random.Random()
+t.title('抽号')
+font = Font(size=20)
+res = Label(t, font=font)
+file = '4班.txt'
+url = 'https://samas3.github.io/chouhao/4班.txt'
+if 'TEMP' not in os.environ:
+    messagebox.showwarning('警告', '找不到缓存目录，缓存不可用')
+    cache = r'\Not Enabled/'
+else:
+    cache = os.environ['TEMP'] + '/chtmp'
+res.grid(row=0, column=0)
+path = Label(t, font=font)
+path.grid(row=1, column=0)
+names = {}
+gnames = {}
+mode = 1
+voice = IntVar()
+def probab(items, probs):
+    it = list(items)
+    pr = list(probs)
+    pr2 = pr.copy()
+    x = int(random.random() * sum(pr2))
+    for i in range(1, len(pr)):
+        pr2[i] += pr2[i - 1]
+    pr2.insert(0, 0)
+    pr2.append(1e100)
+    for i in range(len(pr) + 1):
+        if x >= pr2[i] and x < pr2[i + 1]:
+            return it[i], pr[i]
+def set_width(label, txt):
+    label['width'] = int(font.measure(txt) / 15)
+    label['text'] = txt
+def decrypt_content(s):
+    t = test.decrypt(s, 'Let QwQ bless me')
+    stringio = io.StringIO(t)
+    reader = csv.reader(stringio)
+    maxx = -1
+    for i in reader:
+        if i[2] != '0' and 'g' not in i[0]:
+            k = i[0] + ' ' + i[1]
+            names[k] = int(i[2])
+        elif i[2] != '0' and 'g' in i[0]:
+            k = '第' + i[0][1:] + '组 ' + i[1]
+            gnames[k] = int(i[2])
+        else:
+            continue
+        maxx = max(maxx, int(font.measure(k) / 10))
+    return maxx
+def loadFile():
+    names.clear()
+    try:
+        load()
+    except Exception as e:
+        fail()
+        # raise e
+    else:
+        res['text'] = '初始化完成'
+        res['fg'] = 'black'
+        do['state'] = 'normal'
+        do2['state'] = 'normal'
+def getName(dic):
+    global flag
+    name = probab(dic.keys(), dic.values())
+    flag = 1
+    if voice.get():
+        def _():
+            global flag
+            try:
+                pyttsx3.speak(name[0].split(' ')[1])
+            except:
+                flag = 0
+                messagebox.showinfo('提示', '你先别急')
+        threading.Thread(target=_).start()
+    if flag:
+        return name[0]
+    return -1
+def pickn():
+    global names, mode
+    if len(names) > 0 and mode == 1:
+        n = getName(names)
+        if n != -1:
+            res['text'] = n
+            del names[n]
+    else:
+        mode = 1
+        loadFile()
+do = Button(t, text='抽号(学号)', command=pickn, font=font)
+do.grid(row=2, column=0)
+def pickg():
+    global gnames, mode
+    if len(gnames) > 0 and mode == 2:
+        n = getName(gnames)
+        if n != -1:
+            res['text'] = n
+            del gnames[n]
+    else:
+        mode = 2
+        loadFile()
+do2 = Button(t, text='抽号(小组)', command=pickg, font=font)
+do2.grid(row=3, column=0)
+def fail():
+    res['text'] = '读取失败'
+    res['fg'] = 'red'
+    do['state'] = 'disabled'
+    do2['state'] = 'disabled'
+def load():
+    global path
+    fn = file
+    success = True
+    online = False
+    if os.path.exists(cache):
+        fn = cache
+    if os.path.exists(fn):
+        online = False
+        if fn == cache:
+            online = True
+        with open(fn, 'r') as f:
+            s = f.read()
+            res['width'] = decrypt_content(s)
+    else:
+        try:
+            r = requests.get(url)
+        except:
+            if not os.path.exists(fn):
+                success = False
+                path['text'] = '没有任何文件，请联网下载'
+        if success:
+            online = True
+            if r.status_code == 200:
+                with open(cache, 'w') as f:
+                    f.write(r.text)
+                res['width'] = decrypt_content(r.text)
+            else:
+                fail()
+        else:
+            fail()
+            raise Exception
+    if online:
+        path['text'] = '使用在线文件（本地已缓存）'
+    else:
+        path['text'] = '使用本地文件: ' + fn.split('/')[-1]
+def choose_file_deprecated():
+    global file
+    fn = filedialog.askopenfilename(initialdir='.', filetypes=[('txt', '*.txt')])
+    if fn:
+        file = fn
+        loadFile()
+        path['text'] = '当前文件: ' + fn.split('/')[-1]
+def choose_url():
+    t2 = Toplevel()
+    t2.title('输入URL')
+    t2.wm_attributes('-topmost', 1)
+    txt = Text(t2)
+    txt.pack()
+    def seturl():
+        global url
+        url = txt.get(1.0, 'end').strip()
+        loadFile()
+        t2.destroy()
+    t2.protocol('WM_DELETE_WINDOW', seturl)
+    t2.mainloop()
+Button(t, text='选择学号文件', command=choose_url, font=font).grid(row=4, column=0)
+Checkbutton(t, text='语音', font=font, variable=voice).grid(row=5, column=0)
+def main():
+    loadFile()
+    t.mainloop()
+main()
---
+++ chouhao/test.py
@@ -0,0 +1,31 @@
+from itertools import cycle
+import base64
+
+def crypt(source, key):
+    result = ''
+    temp = cycle(key)
+    for ch in source:
+        result = result + chr(ord(ch) ^ ord(next(temp)))
+        #result += hex(ord(ch) ^ ord(next(temp)))[2:].zfill(4)
+    result = base64.b64encode(result.encode())
+    return result.decode()
+def decrypt(source, key):
+    '''s = source
+    new = ''
+    while s:
+        new += chr(int(s[:4], base=16))
+        s = s[4:]
+    source = new'''
+    result = ''
+    temp = cycle(key)
+    source = base64.b64decode(source).decode()
+    for ch in source:
+        result = result + chr(ord(ch) ^ ord(next(temp)))
+    return result
+if __name__ == '__main__':
+    with open('1班.txt', 'r', encoding='utf-8') as f:
+        with open('4班.txt', 'w', encoding='utf-8') as g:
+            g.write(crypt(f.read(), 'Let QwQ bless me'))
+    with open('4班.txt', 'r', encoding='utf-8') as f:
+        with open('2班.txt', 'w', encoding='utf-8') as g:
+            g.write(decrypt(f.read(), 'Let QwQ bless me'))
